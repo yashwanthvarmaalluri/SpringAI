0697b41b7d4f5ac45ae477147cd225522c366980
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-22 15:44:33 -0400

    Ollama tinyllama model

--- src/main/java/com/yash/SpringAIDemo/OpenAIController.java
+++ src/main/java/com/yash/SpringAIDemo/OpenAIController.java
@@ -1,50 +1,50 @@
-package com.yash.SpringAIDemo;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class OpenAIController {
-
-    private ChatClient chatClient;
-
-//    public OpenAIController(OpenAiChatModel openAiChatModel) {
-//        this.chatClient = ChatClient.create(openAiChatModel);
+//package com.yash.SpringAIDemo;
+//
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+//import org.springframework.ai.chat.memory.ChatMemory;
+//import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+//import org.springframework.ai.chat.model.ChatResponse;
+//import org.springframework.ai.openai.OpenAiChatModel;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class OpenAIController {
+//
+//    private ChatClient chatClient;
+//
+////    public OpenAIController(OpenAiChatModel openAiChatModel) {
+////        this.chatClient = ChatClient.create(openAiChatModel);
+////    }
+//
+////    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+////
+////    public OpenAIController(ChatClient.Builder builder) {
+////        this.chatClient = builder
+////                .defaultAdvisors(MessageChatMemoryAdvisor
+////                        .builder(chatMemory)
+////                        .build())
+////                .build();
+////
+////    }
+//
+////    @GetMapping("api/{message}")
+//    public ResponseEntity<String> GetResponse(@PathVariable String message) {
+//        ChatResponse chatResponse = chatClient
+//                .prompt(message)
+//                .call().chatResponse();
+//
+//        System.out.println("Model is  " + chatResponse.getMetadata().getModel());
+//
+//        String response = chatResponse
+//                .getResult()
+//                .getOutput()
+//                .getText();
+//
+//
+//        return ResponseEntity.ok(response);
 //    }
-
-    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
-
-    public OpenAIController(ChatClient.Builder builder) {
-        this.chatClient = builder
-                .defaultAdvisors(MessageChatMemoryAdvisor
-                        .builder(chatMemory)
-                        .build())
-                .build();
-
-    }
-
-    @GetMapping("api/{message}")
-    public ResponseEntity<String> GetResponse(@PathVariable String message) {
-        ChatResponse chatResponse = chatClient
-                .prompt(message)
-                .call().chatResponse();
-
-        System.out.println("Model is  " + chatResponse.getMetadata().getModel());
-
-        String response = chatResponse
-                .getResult()
-                .getOutput()
-                .getText();
-
-
-        return ResponseEntity.ok(response);
-    }
-}
+//}
