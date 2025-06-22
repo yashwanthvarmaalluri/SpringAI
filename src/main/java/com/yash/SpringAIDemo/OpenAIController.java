3d9e42f50bbe918b35950e10633b965fec610df6
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-21 21:05:06 -0400

    Second commit

---
+++ src/main/java/com/yash/SpringAIDemo/OpenAIController.java
@@ -0,0 +1,23 @@
+package com.yash.SpringAIDemo;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenAIController {
+
+    private OpenAiChatModel openAiChatModel;
+
+    public OpenAIController(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+    }
+
+    @GetMapping("api/{message}")
+    public String GetResponse(@PathVariable String message) {
+        String response = openAiChatModel.call(message);
+        return response;
+    }
+}
