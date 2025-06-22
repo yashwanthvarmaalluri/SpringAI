f810ad0d5cd5fc355950db7630c3f94c484289cd
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-21 21:23:27 -0400

    Asking question to AI model

--- src/main/java/com/yash/SpringAIDemo/OpenAIController.java
+++ src/main/java/com/yash/SpringAIDemo/OpenAIController.java
@@ -1,7 +1,6 @@
 package com.yash.SpringAIDemo;
 
 import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
