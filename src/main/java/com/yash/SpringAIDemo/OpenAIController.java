a4ba929eeeea250ca652c4f4d1ae392e78f7cf64
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 14:02:15 -0400

    Implementing embeddings and cosine similarity

--- src/main/java/com/yash/SpringAIDemo/OpenAIController.java
+++ src/main/java/com/yash/SpringAIDemo/OpenAIController.java
@@ -88,4 +88,21 @@ public class OpenAIController {
         return embeddingModel.embed(text);
 
     }
+    @PostMapping("/api/similarity")
+    public double getSimilarity(@RequestParam String text1, @RequestParam String text2) {
+        float[] embedding1 = embeddingModel.embed(text1);
+        float[] embedding2 = embeddingModel.embed(text2);
+
+        double dotProduct = 0;
+        double norm1 = 0;
+        double norm2 = 0;
+
+        for (int i = 0; i < embedding1.length; i++) {
+            dotProduct += embedding1[i] * embedding2[i];
+            norm1 += Math.pow(embedding1[i], 2);
+            norm2 += Math.pow(embedding2[i], 2);
+        }
+
+        return dotProduct*100 / (Math.sqrt(norm1) * Math.sqrt(norm2));
+    }
 }
