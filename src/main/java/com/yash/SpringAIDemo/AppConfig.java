89e3cad52300551fa3627acecdb7b11ed7a2e6de
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 14:53:05 -0400

    Simple Vector Store and Token text Splitter

---
+++ src/main/java/com/yash/SpringAIDemo/AppConfig.java
@@ -0,0 +1,16 @@
+package com.yash.SpringAIDemo;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+}
---
+++ src/main/java/com/yash/SpringAIDemo/DataInitializer.java
@@ -0,0 +1,32 @@
+package com.yash.SpringAIDemo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Component
+public class DataInitializer {
+
+    @Autowired
+    private VectorStore vectorStore;
+
+    @PostConstruct
+    public void init(){
+
+        TextReader textReader = new TextReader(new ClassPathResource("product_details.txt"));
+        TokenTextSplitter splitter = new TokenTextSplitter(100,30,50,500,false);
+        List<Document> documents = splitter.split(textReader.get());
+
+        vectorStore.add(documents);
+
+    }
+
+}
