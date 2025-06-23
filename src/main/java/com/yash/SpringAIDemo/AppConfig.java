0ba116c220a31fb23810e3c5398808775993b01c
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 16:41:02 -0400

    Pg Vector Store Implementation

--- src/main/java/com/yash/SpringAIDemo/AppConfig.java
+++ src/main/java/com/yash/SpringAIDemo/AppConfig.java
@@ -3,14 +3,16 @@ package com.yash.SpringAIDemo;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
-        return SimpleVectorStore.builder(embeddingModel).build();
+    public VectorStore vectorStore(JdbcTemplate jdbcTemplate,EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(jdbcTemplate,embeddingModel).build();
     }
 }
