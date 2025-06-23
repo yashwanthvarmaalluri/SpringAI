3e9553081d2019ae06b721586cc0fe71ddf8eca1
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 17:17:51 -0400

    RAG Implementation

--- src/main/java/com/yash/SpringAIDemo/OpenAIController.java
+++ src/main/java/com/yash/SpringAIDemo/OpenAIController.java
@@ -2,6 +2,7 @@ package com.yash.SpringAIDemo;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -122,6 +123,14 @@ public class OpenAIController {
 
 
 
+    @PostMapping("api/ask")
+    public String GetResponseWithRag(@RequestParam String query) {
+        return chatClient
+                .prompt(query)
+                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .call()
+                .content();
+    }
 
 
 
