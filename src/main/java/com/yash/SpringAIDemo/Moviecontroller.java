164de32e3507aa8ad4678667b347249410e4b830
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-24 12:56:06 -0400

    BeanListConverter

--- src/main/java/com/yash/SpringAIDemo/Moviecontroller.java
+++ src/main/java/com/yash/SpringAIDemo/Moviecontroller.java
@@ -10,6 +10,7 @@ import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.template.TemplateRenderer;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,6 +65,21 @@ public class Moviecontroller {
     public Movie getMovie(@RequestParam String hero) {
               BeanOutputConverter<Movie> opCon = new BeanOutputConverter<>(Movie.class);
         String message = "best  movie of " + hero  + opCon.getFormat();
+
+        Prompt prompt = new Prompt(message);
+
+        return opCon.convert(chatClient.prompt(prompt).call().content());
+
+
+    }
+
+    @GetMapping("moviesList")
+    public List<Movie> getMovieList(@RequestParam String hero) {
+        BeanOutputConverter<List<Movie>> opCon = new BeanOutputConverter<>(
+                new ParameterizedTypeReference<List<Movie>>() {
+                }
+        );
+        String message = "Really existing Top 5 movies of " + hero  + opCon.getFormat();
 
         Prompt prompt = new Prompt(message);
 
