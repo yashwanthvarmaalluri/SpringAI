41243cf7b08e00a8364bb40991e24a089faeba7d
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 20:57:47 -0400

    Image Generation with Image Options

--- src/main/java/com/yash/SpringAIDemo/ImageGenController.java
+++ src/main/java/com/yash/SpringAIDemo/ImageGenController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,12 @@ public class ImageGenController {
 
     @GetMapping("image/{query}")
     public String generateImage(@PathVariable String query) {
-        ImagePrompt imagePrompt = new ImagePrompt(query);
+        ImagePrompt imagePrompt = new ImagePrompt(query, OpenAiImageOptions.builder()
+                .quality("hd")
+                .height(1024)
+                .width(1024)
+                .style("natural")
+                .build());
         ImageResponse response = openAiImageModel.call(imagePrompt);
 
         return response.getResult().getOutput().getUrl();
