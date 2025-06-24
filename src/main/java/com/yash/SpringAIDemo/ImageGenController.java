6fb5ea9d8d5b7c1fa22a157649e2f0afdf50ab12
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 21:39:12 -0400

    Image describe

--- src/main/java/com/yash/SpringAIDemo/ImageGenController.java
+++ src/main/java/com/yash/SpringAIDemo/ImageGenController.java
@@ -6,9 +6,9 @@ import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageGenController {
@@ -32,4 +32,13 @@ public class ImageGenController {
 
         return response.getResult().getOutput().getUrl();
     }
-}
+
+    @PostMapping("image/describe")
+    public String descImage(@RequestParam String query, @RequestParam MultipartFile file) {
+            return chatClient.prompt()
+                    .user(us -> us.text(query)
+                            .media(MimeTypeUtils.IMAGE_JPEG,file.getResource()))
+                    .call()
+                    .content();
+    }
+    }
