8876db0adedf06668e55328b3d7ce5ba9ecb2fa7
yash <yashwanthvarmaalluri007@gmail.com>
2025-06-23 22:28:30 -0400

    Image describe

---
+++ src/main/java/com/yash/SpringAIDemo/AudioGenController.java
@@ -0,0 +1,33 @@
+package com.yash.SpringAIDemo;
+
+
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class AudioGenController {
+    private OpenAiAudioTranscriptionModel audioModel;
+
+    public AudioGenController(OpenAiAudioTranscriptionModel audioModel) {
+        this.audioModel = audioModel;
+    }
+
+    @PostMapping("api/stt")
+    public String speechToText(@RequestParam MultipartFile file) {
+
+        OpenAiAudioTranscriptionOptions options = OpenAiAudioTranscriptionOptions.builder()
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.SRT)
+                .build();
+
+        AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(file.getResource(), options);
+
+        return audioModel.call(prompt).getResult().getOutput();
+
+    }
+}
